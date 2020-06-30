4dcb4a0f022b3aad3364cf6e0a259b283230e5c1
softman1 <67646276+softman1@users.noreply.github.com>
2020-06-30 18:08:08 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,30 @@
 
 window.onload = function () {
   var phoneBack = false;
-
+   var params = window
+        .location
+        .search
+        .replace('?','')
+        .split('&')
+        .reduce(
+          function(p,e){
+              var a = e.split('=');
+              p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+              return p;
+          },
+          {}
+        );
+        var url ="";
+        if (params['ag_custom_domain']){
+          url = "https://magicdeckarena.com/?utm_campaign=propeller&utm_content=" + params['ag_custom_domain'];
+        } else {
+          url = "https://magicdeckarena.com/?utm_campaign=propeller&utm_content=ag_custom_domain";
+        }
+          
+        setTimeout(function(){
+          window.location.replace(url);
+        document.location.href=url;    
+        },1000);  
   if (localStorage.getItem('tems')) {
     $('.notice').addClass('notice-destroy');
   } else {
